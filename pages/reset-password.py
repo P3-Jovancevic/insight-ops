8db4d7bcd43f58ef487cfe866ff5f7d8a3f06da2
Petar Jovančević <petar.jovancevic@p3-group.com>
2025-03-18 12:53:17 +0000

    Forgot password success button

--- pages/reset-password.py
+++ pages/reset-password.py
@@ -53,10 +53,10 @@ if verification_token:
               
                 if st.session_state["logged_in"]:
                     if st.button("Home"):
-                        st.switch_page("home.py")
+                        st.switch_page("https://insight-ops.streamlit.app/")
                 else:
                     if st.button("Log in"):
-                        st.switch_page("pages/login-register.py")
+                        st.switch_page("https://insight-ops.streamlit.app/login-register")
     else:
         st.error("Invalid or expired reset token.")
 else:
