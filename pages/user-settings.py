fe6c4be5d58bac656a8c486a8f43f725073b2b93
Petar Jovančević <petar.jovancevic@p3-group.com>
2025-04-14 21:13:30 +0000

    usernbame in correct place

--- pages/user-settings.py
+++ pages/user-settings.py
@@ -29,7 +29,6 @@ if st.session_state["logged_in"]:
         st.markdown("### Edit Your Profile")
 
         # Editable fields
-        username = st.text_input("Username", user_doc.get("username", ""))
         org_url = st.text_input("Organization URL", user_doc.get("organization_url", ""))
         project_name = st.text_input("Project Name", user_doc.get("project_name", ""))
         pat = st.text_input("Personal Access Token (PAT)", user_doc.get("pat", ""), type="password")
@@ -43,7 +42,6 @@ if st.session_state["logged_in"]:
                 {"email": user_email.lower()},
                 {
                     "$set": {
-                        "username": username,
                         "organization_url": org_url,
                         "project_name": project_name,
                         "pat": pat
@@ -58,6 +56,8 @@ if st.session_state["logged_in"]:
         
         # Divider line at the bottom
         st.markdown("---")
+
+        username = st.text_input("Username", user_doc.get("username", ""))
 
         if st.button("Update user"):
             update_result = users_collection.update_one(
