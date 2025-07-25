1ed9dacbc0bb9b2f6b702cb7c153215eacf0d0b2
Petar Jovančević <petar.jovancevic@p3-group.com>
2025-07-25 12:42:33 +0000

    test file testing

--- test-ado-connection.py
+++ test-ado-connection.py
@@ -1,13 +1,22 @@
 import requests
+import streamlit as st
 
-personal_access_token = "-"
-organization_url = "https://dev.azure.com/p3ds"
-headers = {"Content-Type": "application/json"}
+# Get credentials from secrets
+personal_access_token = st.secrets["ado"]["ado_pat"]
+organization_url = st.secrets["ado"]["ado_site"]
 
+# Define headers
+headers = {
+    "Content-Type": "application/json"
+}
+
+# Send request to Azure DevOps REST API
 response = requests.get(
     f"{organization_url}/_apis/projects?api-version=7.0",
     auth=("", personal_access_token),
     headers=headers,
 )
 
-print(response.status_code, response.json())
+# Output result in Streamlit app
+st.write("Status Code:", response.status_code)
+st.write("Response JSON:", response.json())
