11db76ecc30f61e679e3a3bcea64ed9d5b6b62de
Petar Jovančević <petar.jovancevic@p3-group.com>
2025-11-06 14:55:57 +0000

    button

--- home.py
+++ home.py
@@ -27,7 +27,7 @@ if not st.session_state["logged_in"]:
     if st.button("Go to Login / Register"):
         st.markdown(
             """
-            <meta content="0; url='https://insight-ops.streamlit.app/login-register'">
+            <meta http-equiv="refresh" content="0; url='https://insight-ops.streamlit.app/login-register'">
             """,
             unsafe_allow_html=True
         )
